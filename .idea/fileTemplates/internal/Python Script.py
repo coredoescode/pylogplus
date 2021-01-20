414f3b45c788082ce21773d33de0747615565212
BuildTools <unconfigured@null.spigotmc.org>
2021-01-19 20:00:15 -0500

    Add config file and add config codec 27 fields (pl+ config import version q27)

---
+++ .idea/fileTemplates/internal/Python Script.py
@@ -0,0 +1,11 @@
+# PyLogPlus
+#
+# file_name.py
+# File description
+#
+# Copyright © 2021 coresdev
+#
+# This product is free software, it is licenced under the GNU General Public Licence v3
+# More information in LICENCE
+#
+# Made with <3 using python
---
+++ PyLogPlus.py
@@ -0,0 +1,18 @@
+# PyLogPlus
+#
+# PyLogPlus.py
+# Main library file for pl+
+#
+# Copyright © 2021 coresdev
+#
+# This product is free software, it is licenced under the GNU General Public Licence v3
+# More information in LICENCE
+#
+# Made with <3 using python
+
+class PyLogPlus:
+
+    def __init__(self):
+        print('PyLogPlus initializing')
+        # Create the dictionary
+        print('PyLogPlus is ready')
